bec89a6833a8a35e32a7b8f3256af8575ff75c6b
weiya <wave.weiya@gmail.com>
2022-04-20 17:48:53 +0800

    algo: add water

---
+++ leetcode/11.盛最多水的容器.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ *
+ * https://leetcode-cn.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (61.49%)
+ * Likes:    3421
+ * Dislikes: 0
+ * Total Accepted:    704.2K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：height = [1,1]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let left = 0,
+    right = height.length - 1;
+
+  let maxWater = 0;
+
+  while (left < right) {
+    const leftSize = height[left];
+    const rightSize = height[right];
+
+    const current = Math.min(leftSize, rightSize) * (right - left);
+
+    maxWater = Math.max(current, maxWater);
+
+    if (leftSize > rightSize) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return maxWater;
+};
+// @lc code=end
