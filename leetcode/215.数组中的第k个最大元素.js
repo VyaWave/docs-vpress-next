e19ad65eb4b43c401376d6aafcb8734171b36ec3
weiya <wave.weiya@gmail.com>
2022-04-22 15:03:44 +0800

    feature: max k char

---
+++ leetcode/215.数组中的第k个最大元素.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=javascript
+ *
+ * [215] 数组中的第K个最大元素
+ *
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (64.70%)
+ * Likes:    1627
+ * Dislikes: 0
+ * Total Accepted:    603.9K
+ * Total Submissions: 933.4K
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+ *
+ * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * -10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  const array = nums.sort((a, b) => b - a).filter((_, i) => i == k - 1);
+
+  return array[0];
+};
+// @lc code=end
